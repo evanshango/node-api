f88f1a30123897590e491f6c05d04f04a4a4e35c
Evans Shango <lutatwaevans@gmail.com>
2020-02-15 22:25:16 +0300

    add a separate route to fetch user photo

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {userById, allUsers, getUser, updateUser, deleteUser} = require('../controllers/userController');
+const {userById, allUsers, getUser, updateUser, deleteUser, userPhoto} = require('../controllers/userController');
 const {requireSignin} = require('../controllers/authController');
 
 const router = express.Router();
@@ -7,6 +7,8 @@ router.get('/users', allUsers);
 router.get('/users/:userId', requireSignin, getUser);
 router.put ('/users/update/:userId', requireSignin, updateUser);
 router.delete ('/users/delete/:userId', requireSignin, deleteUser);
+//photo route
+router.get('/user/photo/:userId', userPhoto);
 
 router.param('userId', userById);
 
