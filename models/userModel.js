270e6a11a94663b7bfe81ce7444970f49930fa57
Evans Shango <lutatwaevans@gmail.com>
2020-02-16 14:41:07 +0300

    handle user following and unfollowing

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const uuidv1 = require('uuid/v1');
 const crypto = require('crypto');
+const {ObjectId} = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
     name: {type: String, trim: true, required: true},
@@ -9,14 +10,10 @@ const userSchema = new mongoose.Schema({
     salt: String,
     created: {type: Date, default: Date.now()},
     updated: Date,
-    photo: {
-        data: Buffer,
-        contentType: String
-    },
-    about: {
-        type: String,
-        trim: true
-    }
+    photo: {data: Buffer, contentType: String},
+    about: {type: String, trim: true},
+    following: [{type: ObjectId, ref: 'User'}],
+    followers: [{type: ObjectId, ref: 'User'}]
 });
 
 //virtual field
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const {userById, allUsers, getUser, updateUser, deleteUser, userPhoto} = require('../controllers/userController');
+const {
+    userById, allUsers, getUser, updateUser, deleteUser, userPhoto, addFollowing, addFollower,
+    removeFollowing, removeFollower
+} = require('../controllers/userController');
 const {requireSignin} = require('../controllers/authController');
 
 const router = express.Router();
+
+router.put('/user/follow', requireSignin, addFollowing, addFollower);
+router.put('/user/unfollow', requireSignin, removeFollowing, removeFollower);
+
 router.get('/users', allUsers);
 router.get('/users/:userId', requireSignin, getUser);
-router.put ('/users/update/:userId', requireSignin, updateUser);
-router.delete ('/users/delete/:userId', requireSignin, deleteUser);
+router.put('/users/update/:userId', requireSignin, updateUser);
+router.delete('/users/delete/:userId', requireSignin, deleteUser);
 //photo route
 router.get('/user/photo/:userId', userPhoto);
 
